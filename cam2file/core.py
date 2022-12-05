e69fb4346b0aa21bf7162b06f231eddbf5500055
rtg0530 <rtg0530@gmail.com>
2022-12-05 21:29:32 +0900

    Detect paper using webcam

---
+++ cam2file/core.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while cv2.waitKey(33) & 0xFF != ord('a'):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    ret, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 400: 
+            approx = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True), True)
+            if(len(approx) == 4): 
+                cv2.drawContours(frame, [approx], 0, (1, 194, 252), 4)
+                break
+    cv2.imshow("frame", frame)
+
+cap.release()
+cv2.destroyAllWindows()
+
